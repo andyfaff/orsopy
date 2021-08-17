3dd89b3d2af1dafbd7128f48944f1c1b21233961
Artur Glavic <artur.glavic@psi.ch>
2021-08-17 11:47:48 +0200

    All Header objects use repr that hides optional arguments that are empty.

--- orsopy/fileio/base.py
+++ orsopy/fileio/base.py
@@ -36,7 +36,6 @@ def __datetime_representer(dumper, data):
 
 yaml.add_representer(datetime.datetime, __datetime_representer)
 
-
 class Header:
     """
     The super class for all of the items in the orso module.
@@ -177,6 +176,17 @@ class Header:
             if not unit.isascii():
                 raise ValueError("The unit must be in ASCII text.")
 
+    def __repr__(self):
+        # representation that does not show empty arguments
+        out = f'{self.__class__.__name__}('
+        for fi in fields(self):
+            if fi.name in self._orso_optionals and getattr(self, fi.name) is None:
+                # ignore empty optional arguments
+                continue
+            out += f'{fi.name}={getattr(self, fi.name).__repr__()}, '
+        out = out[:-2]+')'
+        return out
+
     def _staggered_repr(self):
         """
         Generate a string representation distributed over multiple lines
@@ -187,6 +197,9 @@ class Header:
         slen = len(self.__class__.__name__)
         out = f'{self.__class__.__name__}(\n'
         for fi in fields(self):
+            if fi.name in self._orso_optionals and getattr(self, fi.name) is None:
+                # ignore empty optional arguments
+                continue
             nlen = len(fi.name)
             ftxt = getattr(self, fi.name).__repr__()
             ftxt = ftxt.replace('\n', '\n' + ' ' * (slen + nlen + 2))
@@ -195,7 +208,7 @@ class Header:
         return out
 
 
-@dataclass
+@dataclass(repr=False)
 class Value(Header):
     """A value or list of values with an optional unit."""
 
@@ -205,7 +218,7 @@ class Value(Header):
     )
 
 
-@dataclass
+@dataclass(repr=False)
 class ValueRange(Header):
     """A range or list of ranges with mins, maxs, and an optional unit."""
 
@@ -216,7 +229,7 @@ class ValueRange(Header):
     )
 
 
-@dataclass
+@dataclass(repr=False)
 class ValueVector(Header):
     """A vector or list of vectors with an optional unit.
 
@@ -240,14 +253,14 @@ class ValueVector(Header):
     )
 
 
-@dataclass
+@dataclass(repr=False)
 class Comment(Header):
     """A comment."""
 
     comment: str
 
 
-@dataclass
+@dataclass(repr=False)
 class Person(Header):
     """Information about a person, including name, affilation(s), and email."""
 
@@ -258,13 +271,13 @@ class Person(Header):
     )
 
 
-@dataclass
+@dataclass(repr=False)
 class Creator(Person):
     time: datetime.datetime = None
     computer: str = ""
 
 
-@dataclass
+@dataclass(repr=False)
 class Column(Header):
     """Information about a data column"""
 
@@ -277,7 +290,7 @@ class Column(Header):
     )
 
 
-@dataclass
+@dataclass(repr=False)
 class File(Header):
     """A file with a last modified timestamp."""
 
